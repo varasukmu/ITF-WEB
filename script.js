7ba266ac5c3c6fdacbd31540b23a001389143024
Varaporn Sukmuang <wrp.sukmuang@gmail.com>
2024-10-13 14:42:43 +0700

    Contain Input

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function setName() {
     let username = document.getElementById("name");
     let inputname = document.getElementById("newname");
-    username.textContent = inputname.value + ", OK"
+    username.textContent = inputname.value + ", 67070159"
+    document.getElementById('newname').value = '';
 }
 
 function setPic() {
     let userpic = document.getElementById("pic");
     let inputpic = document.getElementById("newPic");
     userpic.style.backgroundImage = "url(" + inputpic.value + ")";
+    document.getElementById('newPic').value = '';
 }
